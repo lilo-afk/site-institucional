745d92b74e1f9eca70e8c18687bfaecccec50b64
lilo-afk <mma.murilo@gmail.com>
2025-08-17 20:56:05 -0300

    feat(responsividade): ajuste no menu e adição de estilos mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
-window.addEventListener("scroll", function() {
-    const header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.classList.add("shrink");
-    } else {
-        header.classList.remove("shrink");
-    }
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
 });
 
-
 const root = document.documentElement;
-const header = document.querySelector('header');
+const header = document.querySelector("header");
 
 function syncHeaderHeight() {
-  root.style.setProperty('--header-h', header.offsetHeight + 'px');
+  root.style.setProperty("--header-h", header.offsetHeight + "px");
 }
-window.addEventListener('load', syncHeaderHeight);
-window.addEventListener('resize', syncHeaderHeight);
-window.addEventListener('scroll', syncHeaderHeight); 
+window.addEventListener("load", syncHeaderHeight);
+window.addEventListener("resize", syncHeaderHeight);
+window.addEventListener("scroll", syncHeaderHeight);
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+
+  // animação do hambúrguer
+  hamburger.classList.toggle("active");
+});
